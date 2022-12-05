e041ad4e357f7682e3ab3be667bc6b4f2e75e5d4
farhad.p <farhad.p@DESKTOP-HD4J39J>
2022-12-05 15:48:59 +0400

    Add project files.

---
+++ FirstProject/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace FirstProject;
+using System.Text;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.OutputEncoding = Encoding.Unicode;
+        // Azərbaycan şirifti daxil etmək Üçün istifadə
+        // etdim Başqa sinifdə olduğu üçün yuxarda həmin sinifidə çağırdım 
+            Console.Write("Bölünən ədədi daxil edin:");
+        // Console.Write nan yazdım ,çünkü consolda keyboardnan daxil edəndə
+        // tələb elədiyim mətnin qarşısında yazmağ mümükün olsun
+            int a = Convert.ToInt32(Console.ReadLine());
+        // burda keyboard dan daxil etdiyimiz karakteri çevir int32 e  
+            Console.Write("Bölən ədədi daxil edin:");
+            int c = Convert.ToInt32(Console.ReadLine());
+            int Qaliq = a % c;
+        Console.WriteLine(a + " /  " + c + " = " + Qaliq );
+        }
+    }
