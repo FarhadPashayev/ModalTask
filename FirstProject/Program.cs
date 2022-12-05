9207ef7e180b7b4facfd197f6008a36fe7d44d8f
Farhad Pashayev <Farhadip@code.edu.az>
2022-12-05 16:45:16 +0400

    FirstCommit

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -7,15 +7,22 @@ using System.Text;
         {
         Console.OutputEncoding = Encoding.Unicode;
         // Azərbaycan şirifti daxil etmək Üçün istifadə
-        // etdim Başqa sinifdə olduğu üçün yuxarda həmin sinifidə çağırdım 
-            Console.Write("Bölünən ədədi daxil edin:");
+        // etdim Başqa sinifdə olduğu üçün yuxarda həmin sinifidə çağırdım
+        Console.WriteLine("Xoş gəlmisiniz bir kalkulyator təsəvvür edin sadəcə toplayır tanış olmağ üçün enter basmağ kifayətdir");
+            var userInput = Console.ReadKey();
+        
+            
+           if (userInput.Key == ConsoleKey.Enter) {
+            Console.Write("1-ci Rəqəmi daxil edin:");
+            }
+           
         // Console.Write nan yazdım ,çünkü consolda keyboardnan daxil edəndə
         // tələb elədiyim mətnin qarşısında yazmağ mümükün olsun
             int a = Convert.ToInt32(Console.ReadLine());
         // burda keyboard dan daxil etdiyimiz karakteri çevir int32 e  
-            Console.Write("Bölən ədədi daxil edin:");
+            Console.Write("2-ci ədədi daxil edin:");
             int c = Convert.ToInt32(Console.ReadLine());
-            int Qaliq = a % c;
-        Console.WriteLine(a + " /  " + c + " = " + Qaliq );
+            int Sum = a + c;
+        Console.WriteLine(a + " +  " + c + " = " + Sum );
         }
     }
